a54987677025e169a46bbd07c4736e084c11b55d
mpppk <niboshiporipori@gmail.com>
2024-12-18 02:53:53 +0900

    Add newFetch

--- example-projects/vite/src/github/client.ts
+++ example-projects/vite/src/github/client.ts
@@ -1,52 +1,8 @@
-import type FetchT from "@notainc/typed-api-spec/fetch";
-import type { InvalidResponseSpec, Spec } from "./spec.ts";
+import { newFetch } from "@notainc/typed-api-spec/zod/validation";
 export const GITHUB_API_ORIGIN = "https://api.github.com";
 
-type FetchGitHub = FetchT<typeof GITHUB_API_ORIGIN, Spec>;
-let fetchGitHub = fetch as FetchGitHub
-// if (import.meta.env.DEV) {
-//   const { withValidation } = await import("@notainc/typed-api-spec/fetch");
-//   const { ZodSpec } = await import("./spec.ts");
-//   const { newZodValidator } = await import("@notainc/typed-api-spec");
-//   const v = newZodValidator(ZodSpec);
-//   fetchGitHub = withValidation(
-//     fetch,
-//     ZodSpec,
-//     v.req,
-//     v.res,
-//   ) as typeof fetchGitHub;
-// }
-if (import.meta.env.DEV) {
-  const { withZodValidation } = await import("@notainc/typed-api-spec/zod/validation");
-  const { ZodSpec } = await import("./spec.ts");
-  fetchGitHub = await withZodValidation(ZodSpec) as typeof fetchGitHub
-}
+const newFetchGitHub = newFetch(async () => (await import("./spec.ts")).ZodSpec, import.meta.env.DEV);
+export const fetchGitHub = await newFetchGitHub<typeof GITHUB_API_ORIGIN>();
 
-// const newFetchGitHub = async (): Promise<FetchGitHub> => {
-//   if (import.meta.env.DEV) {
-//     const { withZodValidation } = await import("@notainc/typed-api-spec/zod/validation");
-//     const { ZodSpec } = await import("./spec.ts");
-//     return withZodValidation(ZodSpec)
-//   }
-//   return fetch
-// }
-
-let fetchInvalidResponseGitHub = fetch as FetchT<
-  typeof GITHUB_API_ORIGIN,
-  InvalidResponseSpec
->;
-if (import.meta.env.DEV) {
-  const { withValidation } = await import("@notainc/typed-api-spec/fetch");
-  const { InvalidResponseZodSpec } = await import("./spec.ts");
-  const { newZodValidator } = await import("@notainc/typed-api-spec");
-  const v = newZodValidator(InvalidResponseZodSpec);
-  fetchInvalidResponseGitHub = withValidation(
-    fetch,
-    InvalidResponseZodSpec,
-    v.req,
-    v.res,
-  ) as typeof fetchInvalidResponseGitHub;
-}
-
-// export const fetchGitHub = newFetchGitHub();
-export { fetchGitHub, fetchInvalidResponseGitHub };
+const newFetchInvalidResponseGitHub = newFetch( async () => (await import("./spec.ts")).InvalidResponseZodSpec, import.meta.env.DEV);
+export const fetchInvalidResponseGitHub = await newFetchInvalidResponseGitHub<typeof GITHUB_API_ORIGIN>();
--- pkgs/typed-api-spec/src/zod/validation.ts
+++ pkgs/typed-api-spec/src/zod/validation.ts
@@ -1,8 +1,25 @@
-import type { ZodApiEndpoints } from "./index";
+import type { ToApiEndpoints, ZodApiEndpoints } from "./index";
+import type FetchT from "../fetch/index.js";
 
 export const withZodValidation = async (spec: ZodApiEndpoints, ft = fetch) => {
   const { withValidation } = await import("../fetch/index.js");
   const { newZodValidator } = await import("./index.js");
   const v = newZodValidator(spec);
   return withValidation(ft, spec, v.req, v.res);
+};
+
+export const newFetch = <Spec extends ZodApiEndpoints = ZodApiEndpoints>(
+  specLoader: () => Promise<Spec>,
+  validation: boolean,
+  ft = fetch,
+) => {
+  return async <
+    Origin extends string = "",
+    Fetch = FetchT<Origin, ToApiEndpoints<Spec>>,
+  >(): Promise<Fetch> => {
+    if (validation) {
+      return (await withZodValidation(await specLoader(), ft)) as Fetch;
+    }
+    return ft as Fetch;
+  };
 };
